705c6a8bb1d966b05b865e0fe442e9dfb9d8b046
1f10210172 <s1f102101727@iniad.org>
2023-01-07 17:41:36 +0900

    追加・諸々変更（このままだとrunserverに影響が出る）

--- diary/urls.py
+++ diary/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views
 
+app_name = 'diary'
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.IndexView.as_view(), name="index"),
     path("diary/", views.diary, name="diary"),
     path("whather/", views.weather, name="weather"),
     path("schedule/", views.schedule, name="schedule"), 
+    path("diary/create/", views.DiaryCreateView.as_view(), name='diary_create'),
+    path("diary/create/complete/", views.DiaryCreateCompleteView.as_view(), name='diary_create_complete'),
 ]
--- diary/views.py
+++ diary/views.py
@@ -1,14 +1,28 @@
 from django.shortcuts import render
+from . import views
+from django.views.generic import TemplateView
+from django.views.generic import CreateView
+from django import forms
+from .models import Diary
 
 # Create your views here.
+
+class IndexView(TemplateView):
+  template_name = 'index.html'
+
 def weather(request):
   return render(request,'diary/weather.html')
 
 def diary(request):
   return render(request, 'diary/diary.html')
 
-def index(request):
-  return render(request, 'diary/index.html')
-
 def schedule(request):
   return render(request, 'diary/schedule.html')
+
+class DiaryCreateView(CreateView):
+  template_name = 'diary_create.html'
+  form_class = DiaryForm
+  success_url = reverse_lazy('diary:diary_create_complete')
+
+class DiaryCreateCompleteView(TemplateView):
+  template_name = 'diary_create_complete.html'
