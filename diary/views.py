98acfbf9934ec85eddbec88469596b01adb488ac
1f10210172 <s1f102101727@iniad.org>
2023-01-07 18:16:21 +0900

    修正

--- diary/views.py
+++ diary/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.views.generic import CreateView
+from django.urls import reverse_lazy
 
 # Create your views here.
 
