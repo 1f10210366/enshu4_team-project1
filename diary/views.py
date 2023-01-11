8b6f8564eeaa0bc7086e0af58528bb2f2ad160d7
1f10210172 <s1f102101727@iniad.org>
2023-01-11 18:53:08 +0900

    機能の追加

--- diary/views.py
+++ diary/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from django.views.generic import TemplateView
 from django.views.generic import CreateView
 from django.views.generic import ListView
 from django.views.generic import DetailView
+from django.views.generic import UpdateView
+from django.views.generic import DeleteView
 from django.urls import reverse_lazy
 from .forms import DiaryForm
 from .models import Diary
+from django.utils import timezone
 
 # Create your views here.
 
@@ -36,3 +40,27 @@ class DiaryListView(ListView):
 class DiaryDetailView(DetailView):
     template_name = 'diary/diary_detail.html'
     model = Diary
+
+class DiaryUpdateView(UpdateView):
+    template_name = 'diary_update.html'
+    model = Diary
+    fields = ('date', 'title', 'text',)
+    success_url = reverse_lazy('diary:diary_list')
+
+    def form_valid(self, form):
+        diary = form.save(commit=False)
+        diary.updated_at = timezone.now()
+        diary.save()
+        return super().form_valid(form)
+
+class DiaryDeleteView(DeleteView):
+    template_name = 'diary/diary_delete.html'
+    model = Diary
+    success_url = reverse_lazy('diary:diary_list')
+
+    def delete(self, request, *args, **kwargs):
+    #Call the delete() method on the fetched object and then redirect to thesuccess URL.
+      self.object = self.get_object()
+      success_url = self.get_success_url()
+      self.object.delete()
+      return HttpResponseRedirect(success_url)
