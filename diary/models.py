0d7f8ba6cb06871feef41447c3b7e09099d2fdf6
s1f10210366 <s1f102103662@iniad.org>
2023-09-03 15:34:04 +0900

    修正

--- diary/models.py
+++ diary/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 import uuid
+from django.contrib.auth.models import User  # ユーザーモデルをインポート
 #ログインユーザごとにデータの切り替え
 
 
@@ -12,4 +13,4 @@ class Diary(models.Model):
     text = models.CharField(verbose_name='本文', max_length=400)
     created_at = models.DateTimeField(verbose_name='作成日時', default=timezone.now)
     updated_at = models.DateTimeField(verbose_name='編集日時', blank=True, null=True)
-    
+    contributor = models.ForeignKey(User, on_delete=models.CASCADE)  # 投稿者を表す外部キー
