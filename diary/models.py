d97f6731165c0abf5cf1ec5820225e13e84a41de
s1f10210366 <s1f102103662@iniad.org>
2023-09-04 14:03:26 +0900

    Update models.py

--- diary/models.py
+++ diary/models.py
@@ -8,6 +8,6 @@ class Diary(models.Model):
     title = models.CharField(verbose_name='タイトル', max_length=40)
     text = models.CharField(verbose_name='本文', max_length=400)
     created_at = models.DateTimeField(verbose_name='作成日時', default=timezone.now)
-
     updated_at = models.DateTimeField(verbose_name='編集日時', blank=True, null=True)
-    
+
+    contributer = models.CharField(max_length=50, null=True) 
