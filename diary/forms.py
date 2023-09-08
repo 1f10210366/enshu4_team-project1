8b9c7628898930ae665480f83ab95c5764bdcb67
s1f10210366 <s1f102103662@iniad.org>
2023-09-08 20:23:34 +0900

    スケジュール

--- diary/forms.py
+++ diary/forms.py
@@ -27,3 +27,15 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ["username", "email", "password1", "password2"]
+
+
+class EventForm(forms.Form):
+
+    start_date = forms.IntegerField(required=True)
+    end_date = forms.IntegerField(required=True)
+    event_name = forms.CharField(required=True, max_length=32)
+
+class CalendarForm(forms.Form):
+
+    start_date = forms.IntegerField(required=True)
+    end_date = forms.IntegerField(required=True)
