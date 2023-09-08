a15a454212f558ac63a9d6d94cc2233457971d95
s1f10210366 <s1f102103662@iniad.org>
2023-09-08 20:42:21 +0900

    Update urls.py

--- diary/urls.py
+++ diary/urls.py
@@ -11,11 +11,14 @@ urlpatterns = [
     #path("", views.IndexView.as_view(), name="index"),
     path("diary/", views.diary, name="diary"),
     path("whather/", views.weather, name="weather"),
-    path("schedule/", views.schedule, name="schedule"), 
+    path("schedule/", views.schedule, name="schedule"),
+    path("add/", views.add_event, name="add_event"),
+    path("list/", views.get_events, name="get_events"), 
     path("diary/create/", views.DiaryCreateView.as_view(), name='diary_create'),
     path("diary/create/complete/", views.DiaryCreateCompleteView.as_view(), name='diary_create_complete'),
     path('diary/list/', views.DiaryListView.as_view(), name='diary_list'),
     path('diary/detail/<uuid:pk>/', views.DiaryDetailView.as_view(), name='diary_detail'),
     path('diary/update/<uuid:pk>/', views.DiaryUpdateView.as_view(), name='diary_update'),
     path('diary/delete/<uuid:pk>/', views.DiaryDeleteView.as_view(), name='diary_delete'),
+
 ]
