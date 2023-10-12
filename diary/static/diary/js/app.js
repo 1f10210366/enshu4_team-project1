1a7d6c36eb4ab39c569d008c7b9620a947b1a2c7
s1f10210366 <s1f102103662@iniad.org>
2023-10-12 13:22:04 +0900

    修正

--- diary/static/diary/js/app.js
+++ diary/static/diary/js/app.js
@@ -22,7 +22,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 
                 // 登録処理の呼び出し
                 axios
-                    .post("/sc/add/", {
+                    .post("/dairy/add/", {
                         start_date: info.start.valueOf(),
                         end_date: info.end.valueOf(),
                         event_name: eventName,
@@ -47,7 +47,7 @@ document.addEventListener('DOMContentLoaded', function () {
         events: function (info, successCallback, failureCallback) {
 
             axios
-                .post("/sc/list/", {
+                .post("/diary/list/", {
                     start_date: info.start.valueOf(),
                     end_date: info.end.valueOf(),
                 })
