1b305237dd3c9e6ea018285ea9e3d53a17bac387
s1f10210366 <s1f102103662@iniad.org>
2023-10-12 14:00:26 +0900

    修正

--- diary/views.py
+++ diary/views.py
@@ -148,40 +148,40 @@ def add_event(request):
     イベント登録
     """
 
-    if request.method == "GET":
-        # GETは対応しない
-        raise Http404()
+    if request.method == "POST":
+        # JSONの解析
+        datas = json.loads(request.body)
 
-    # JSONの解析
-    datas = json.loads(request.body)
+        # バリデーション
+        eventForm = EventForm(datas)
+        if eventForm.is_valid():
 
-    # バリデーション
-    eventForm = EventForm(datas)
-    if eventForm.is_valid() == False:
-        # バリデーションエラー
-        raise Http404()
+            # リクエストの取得
+            start_date = datas["start_date"]
+            end_date = datas["end_date"]
+            event_name = datas["event_name"]
 
-    # リクエストの取得
-    start_date = datas["start_date"]
-    end_date = datas["end_date"]
-    event_name = datas["event_name"]
+            # 日付に変換。JavaScriptのタイムスタンプはミリ秒なので秒に変換
+            formatted_start_date = time.strftime(
+                "%Y-%m-%d", time.localtime(start_date / 1000))
+            formatted_end_date = time.strftime(
+                "%Y-%m-%d", time.localtime(end_date / 1000))
 
-    # 日付に変換。JavaScriptのタイムスタンプはミリ秒なので秒に変換
-    formatted_start_date = time.strftime(
-        "%Y-%m-%d", time.localtime(start_date / 1000))
-    formatted_end_date = time.strftime(
-        "%Y-%m-%d", time.localtime(end_date / 1000))
+            # 登録処理
+            event = Event(
+                event_name=str(event_name),
+                start_date=formatted_start_date,
+                end_date=formatted_end_date,
+            )
+            event.save()
 
-    # 登録処理
-    event = Event(
-        event_name=str(event_name),
-        start_date=formatted_start_date,
-        end_date=formatted_end_date,
-    )
-    event.save()
-
-    # 空を返却
-    return HttpResponse("")
+            # 空を返却
+            return HttpResponse("")
+        else:
+                # バリデーションエラー
+                return JsonResponse({"error": "Validation failed"}, status=400)
+    else:
+        return JsonResponse({"error": "Method Not Allowed"}, status=405)
 
 
 def get_events(request):
@@ -189,43 +189,42 @@ def get_events(request):
     イベントの取得
     """
 
-    if request.method == "GET":
-        # GETは対応しない
-        raise Http404()
+    if request.method == "POST":
+        # JSONの解析
+        datas = json.loads(request.body)
+        # バリデーション
+        calendarForm = CalendarForm(datas)
+        if calendarForm.is_valid():
+            
 
-    # JSONの解析
-    datas = json.loads(request.body)
+            # リクエストの取得
+            start_date = datas["start_date"]
+            end_date = datas["end_date"]
 
-    # バリデーション
-    calendarForm = CalendarForm(datas)
-    if calendarForm.is_valid() == False:
-        # バリデーションエラー
-        raise Http404()
+            # 日付に変換。JavaScriptのタイムスタンプはミリ秒なので秒に変換
+            formatted_start_date = time.strftime(
+                "%Y-%m-%d", time.localtime(start_date / 1000))
+            formatted_end_date = time.strftime(
+                "%Y-%m-%d", time.localtime(end_date / 1000))
 
-    # リクエストの取得
-    start_date = datas["start_date"]
-    end_date = datas["end_date"]
+            # FullCalendarの表示範囲のみ表示
+            events = Event.objects.filter(
+                start_date__lt=formatted_end_date, end_date__gt=formatted_start_date
+            )
 
-    # 日付に変換。JavaScriptのタイムスタンプはミリ秒なので秒に変換
-    formatted_start_date = time.strftime(
-        "%Y-%m-%d", time.localtime(start_date / 1000))
-    formatted_end_date = time.strftime(
-        "%Y-%m-%d", time.localtime(end_date / 1000))
+            # fullcalendarのため配列で返却
+            list = []
+            for event in events:
+                list.append(
+                    {
+                        "title": event.event_name,
+                        "start": event.start_date,
+                        "end": event.end_date,
+                    }
+                )
 
-    # FullCalendarの表示範囲のみ表示
-    events = Event.objects.filter(
-        start_date__lt=formatted_end_date, end_date__gt=formatted_start_date
-    )
-
-    # fullcalendarのため配列で返却
-    list = []
-    for event in events:
-        list.append(
-            {
-                "title": event.event_name,
-                "start": event.start_date,
-                "end": event.end_date,
-            }
-        )
-
-    return JsonResponse(list, safe=False)
+            return JsonResponse(list, safe=False)
+        else:
+            return JsonResponse({"error": "Validation failed"}, status=400)
+    else:
+        return JsonResponse({"error": "Method Not Allowed"}, status=405)
