eac6576771cf5b9523cd01f862df2fdd036cef64
s1f10210366 <s1f102103662@iniad.org>
2023-10-13 10:32:37 +0900

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -100,7 +100,7 @@ else:
     
     db_from_env = dj_database_url.config(conn_max_age=600)
     DATABASES = {
-        'default': dj_database_url.config()
+        'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'), conn_max_age=600)
     }
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
